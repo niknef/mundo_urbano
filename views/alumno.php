9a54bbaf72bac8a3e5ccf4d73cb8cfea633539b3
NikNef <nicolas.firpo@davinci.edu.ar>
2024-10-02 11:07:59 -0300

    add: alumno view

---
+++ views/alumno.php
@@ -0,0 +1,22 @@
+<section class="alumno d-flex align-items-center justify-content-center">
+    <div class="row featurette align-items-center text-center">
+        <div class="col-md-7 order-md-first order-last">
+        <h1 class="featurette-heading fw-light lh-1">Nicolás Firpo</h1>
+        <p class="lead"><em>Nacimiento: 17-04-2000</em></p>
+        <div class="d-flex justify-content-center gap-3">
+            <a href="mailto:nicolas.firpo@davinci.edu.ar" class="badge custom-badge bg-primary">
+            <i class="bi bi-envelope-fill"></i> Mail
+            </a>
+            <a href="https://www.linkedin.com/in/nicolasfirpo/" target="_blank" class="badge custom-badge bg-info">
+            <i class="bi bi-linkedin"></i> LinkedIn
+            </a>
+            <a href="https://github.com/niknef" target="_blank" class="badge custom-badge bg-dark">
+            <i class="bi bi-github"></i> GitHub
+            </a>
+        </div>
+        </div>
+        <div class="col-md-5 order-md-last order-first">
+        <img src="./img/perfil-nicolas-firpo.png" alt="Nicolás Firpo" class="featurette-image img-fluid mx-auto rounded-circle">
+        </div>
+    </div>
+</section>
